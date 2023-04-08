16bbb8dc7a1a0aedbcb63f452b294c0dd5698c72
cmarius46 <cmarius46@gmail.com>
2023-04-08 20:56:44 +0300

    switched to local

--- scripts/ufla/ufla.js
+++ scripts/ufla/ufla.js
@@ -1,9 +1,11 @@
-import {logger}  from './listener_adder.js'
+import {logger}  from './listener_adder.js'; // old; without config
 
+import { initializeLogger } from './listener_adder.js'; // new
 
-// export default function init() {
-// 	console.log("UFLA init");
-// 	logger.init();
-// }
-
-
+export default function init() {
+	console.log("UFLA init");
+	// logger.init();
+    initializeLogger({
+        licenseKey: "pulapulapizdapizda",
+      });
+}
